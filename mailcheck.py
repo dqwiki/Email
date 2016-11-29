3fb3c03b6cb5efaf8e91a3d298ec0b2a273689b0
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2016-11-29 18:21:30 -0500

    Licence

--- mailcheck.py
+++ mailcheck.py
@@ -1,3 +1,20 @@
+"""
+This file is part of DeltaQuadBot.
+
+DeltaQuadBot is free software: you can redistribute it and/or modify
+it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DeltaQuadBot is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DeltaQuadBot. If not, see <https://www.gnu.org/licenses/agpl.txt>.
+"""
+
 # -*-coding: utf-8 -*-
 import sys,imaplib,localconfig,platform
 if platform.system() == "Windows":
